84596815a1788a7e8151974d6907e20df60b5858
Michelle S <progamer.firestorm@gmail.com>
2021-08-13 14:34:03 -0600

    Add implementation

--- src/BlockInitials.java
+++ src/BlockInitials.java
@@ -1,5 +1,21 @@
+@SuppressWarnings("SpellCheckingInspection")
 public class BlockInitials {
     public static void main(String[] args) {
-        // TODO: Add implementation.
+        String[] lines = {
+                "MM              MM    JJJJJJJJJJ      SSSSSS  ",
+                "MMMM          MMMM        JJ        SS      SS",
+                "MM  MM      MM  MM        JJ        SS        ",
+                "MM    MM  MM    MM        JJ        SS        ",
+                "MM      MM      MM        JJ          SSS     ",
+                "MM              MM        JJ             SSS  ",
+                "MM              MM        JJ                SS",
+                "MM              MM        JJ                SS",
+                "MM              MM    JJ  JJ        SS      SS",
+                "MM              MM      JJ            SSSSSS  ",
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
