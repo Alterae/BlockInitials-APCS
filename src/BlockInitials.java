61846906d186b98ec8ab7b882f114001ae5d5df0
Michelle S <progamer.firestorm@gmail.com>
2021-08-13 14:39:07 -0600

    Add documentation

--- src/BlockInitials.java
+++ src/BlockInitials.java
@@ -1,6 +1,13 @@
+// Suppress spell-checking so that the string of Js in the initials doesn't cause a warning.
 @SuppressWarnings("SpellCheckingInspection")
+
 public class BlockInitials {
+    /**
+     * Main function.  Prints the author's initials in block letters.
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args) {
+        // Array literals use curly braces.
         String[] lines = {
                 "MM              MM    JJJJJJJJJJ      SSSSSS  ",
                 "MMMM          MMMM        JJ        SS      SS",
@@ -14,6 +21,7 @@ public class BlockInitials {
                 "MM              MM      JJ            SSSSSS  ",
         };
 
+        // Here's how we do a foreach loop in Java.
         for (String line : lines) {
             System.out.println(line);
         }
